b7e9f186c514b6be1b381796b73a427aa445a3a4
JaiRaga <raga.j.santhosh@gmail.com>
2022-01-17 21:36:02 +0530

    :zap: abstracted the config to seperate location

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,21 +2,14 @@ import dotenv from 'dotenv'
 import { MikroORM } from '@mikro-orm/core'
 import { __prod__ } from './constants'
 import { Post } from './entities/Post'
+import microConfig from './mikro-orm.config'
 
 dotenv.config()
 
 const main = async () => {
 	try {
-		const orm = await MikroORM.init({
-			name: 'redit',
-			type: 'postgresql',
-			dbName: 'reditDB',
-			user: process.env.DB_USER,
-			password: process.env.DB_PASS,
-			entities: [Post],
-			debug: !__prod__,
-		})
-
+		const orm = await MikroORM.init(microConfig)
+		await orm.getMigrator().up()
 		const post = orm.em.create(Post, { itle: 'My First Post' })
 		await orm.em.persistAndFlush(post)
 		console.log('*************************Sql2*******************************')
